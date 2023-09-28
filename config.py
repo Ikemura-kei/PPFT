ceeadfd128380e65a98e7fd61a0a87f427bf0df7
IKEMURA-Kei <ikemurakei2001@gmail.com>
2023-09-29 04:58:37 +0800

    added data percentage term to the HAMMER dataset

--- config.py
+++ config.py
@@ -15,6 +15,8 @@ parser = argparse.ArgumentParser(description='CompletionFormer')
 
 
 # Dataset
+paerser.add_argument('--data_percentage', type=float, default=1.0, help='the percentage of the entire dataset used')
+
 parser.add_argument('--dir_data',
                     type=str,
                     default='./dataset/NYUDepthV2_HDF5',
