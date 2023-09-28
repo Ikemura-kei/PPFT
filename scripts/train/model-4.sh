10c5a6b2a5d8c6809a075bac88604ee15ffa5a51
IKEMURA-Kei <ikemurakei2001@gmail.com>
2023-09-29 00:48:30 +0800

    updated training script to resume

--- scripts/train/model-4.sh
+++ scripts/train/model-4.sh
@@ -21,6 +21,8 @@ python main.py --dir_data /root/autodl-tmp/yiming/datasets/polar_hammer \
                 --pretrained_completionformer /root/autodl-tmp/yiming/PDNE/pretrained/comp/NYUv2.pt \
                 --use_pol \
                 --pol_rep grayscale-4 \
+                --resume --pretrain "/root/autodl-tmp/yiming/ikemura_ws/PDNE_VPT_V2/PDNE/experiments/230928_063056_model-4-1-0/model_00050.pt" \
+
                 # --resume \
                 # --pretrain /root/autodl-tmp/yiming/ikemura_ws/PDNE_VPT/PDNE/experiments/230919_040259_model-2/model_00020.pt \
                 
